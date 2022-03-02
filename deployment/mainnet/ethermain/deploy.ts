0f8d00a82cbdb269439c6b764961d1188a9afbd7
John31337 <95174336+John31337@users.noreply.github.com>
2022-03-03 01:04:25 +0800

    All blockchains

---
+++ deployment/mainnet/ethermain/deploy.ts
@@ -0,0 +1,9 @@
+import { task } from "hardhat/config";
+import { TaskArguments } from "hardhat/types";
+import { deployDiamond } from "../../diamond";
+
+task("ethermain:Diamond", "Deploy Diamond")
+    .setAction(async function (taskArguments: TaskArguments, { ethers, upgrades }) {
+        const crowdFundingDiamond = await deployDiamond(ethers);
+        console.log("Diamond deployed to:", crowdFundingDiamond);
+    });
